218a468599b4108a56b8b71e37266b8dd501a496
marcosox <marcosox@users.noreply.github.com>
2015-09-10 21:59:56 +0200

    Implemented map, added geolocalized dump
    Implementata la mappa, caricato dump con meta' indirizzi geolocalizzati

--- Visualizzazione_incidenti/WebContent/js/map_geo.js
+++ Visualizzazione_incidenti/WebContent/js/map_geo.js
@@ -1,79 +1,68 @@
 var dataset = null;
 var map;
-var infoWindows = []; // in realta' ce ne dovrebbe essere una sola alla volta qui dentro
+var infoWindows = []; // in realta' ce ne dovrebbe essere una sola alla volta
+						// qui dentro
+var markers = [];
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('mapgeo-canvas'), {
-    zoom: 11,
-    center: {lat: 41.893, lng: 12.482}
-  });
-  getGeocodedAccidents();
+	map = new google.maps.Map(document.getElementById('mapgeo-canvas'), {
+		zoom : 11,
+		center : {
+			lat : 41.893,
+			lng : 12.482
+		}
+	});
+	getGeocodedAccidents();
 }
 
 function closeAllInfoWindows() {
 	for (i = 0; i < infoWindows.length; i++) {
 		infoWindows[i].close(); // chiude le infowindow
+		infoWindows[i]= null;
 	}
+	infoWindows = [];
 }
 
-function getGeocodedAccidents(){
+function getGeocodedAccidents() {
 	over = '<div id="overlay"><h1>LOADING</h></div>';
 	$(over).appendTo('#container');
-	
+
 	$.ajax({
 		type : 'POST',
 		url : "GetGeocodedAccidents",
 		dataType : 'json',
 		success : function(result) {
 			dataset = result;
-			// 2 marker d'esempio
-			marker = new google.maps.Marker({
-			    position: {lat: +dataset[0].lat, lng: +dataset[0].lon},
-			    map: map,
-			    title: dataset[0].protocollo,
-			    //icon: 'default.png' oppure
-			    /*var image = {
-    				url: 'images/beachflag.png',
-    				// This marker is 20 pixels wide by 32 pixels high.
-    				size: new google.maps.Size(20, 32),
-    				// The origin for this image is (0, 0).
-    				origin: new google.maps.Point(0, 0),
-    				// The anchor for this image is the base of the flagpole at (0, 32).
-    				anchor: new google.maps.Point(0, 32)
-  };
-
-			       var shape = {
-			    coords: [1, 1, 1, 20, 18, 20, 18, 1],
-			    type: 'poly'
-			  };
-			     
-			    icon: image,
-			      shape: shape, // oggetto shape
-			      zIndex: 3 //int
-*/
-			  });
-			
-			marker.addListener('click', function() {
-				closeAllInfoWindows();
-				infowindow = new google.maps.InfoWindow();
-				contentString = marker.title;
-				infowindow.setContent(contentString);
-			    infowindow.open(map, marker);
-			    infoWindows.push(infowindow);
-			});
-			marker2 = new google.maps.Marker({
-			    position: {lat: +dataset[1].lat, lng: +dataset[1].lon},
-			    map: map,
-			    title: dataset[1].protocollo			    
-			  });
-			
-			marker2.addListener('click', function() {
-				closeAllInfoWindows();
-				infowindow = new google.maps.InfoWindow();
-				contentString = marker2.title;
-				infowindow.setContent(contentString);
-			    infowindow.open(map, marker2);
-			    infoWindows.push(infowindow);
-			});
+			myCircle = google.maps.SymbolPath.CIRCLE;
+			for (i = 0; i < 100/*dataset.length*/; i++) {
+				
+				var marker = new google.maps.Marker({
+					position : {
+						lat : +dataset[i].lat,
+						lng : +dataset[i].lon
+					},
+					map : map,
+					title : "Incidente n."+dataset[i].protocollo,
+					icon: {
+					      path: myCircle,
+					      scale: 1
+					    }
+				});
+				console.log(marker.title);
+				marco = marker;
+				marker.addListener('click', function() {
+					//console.log(this);
+					closeAllInfoWindows();
+					infowindow = new google.maps.InfoWindow();
+					contentString = "<div><b>"+this.title+"</b>"
+					+"<br>Orario: xx"
+					+"</div>";
+					infowindow.setContent(contentString);
+					infowindow.open(map, this);
+					infoWindows.push(infowindow);
+				});
+				markers.push(marker);
+			}
 		},
 		error : function(result) {
 			console.log("Error retrieving geocoded accidents");
